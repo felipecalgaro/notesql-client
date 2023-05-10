b8a4372e4ab836e28aaa83ecc234e6ee66b8264c
Felipe <pipecalgaro@gmail.com>
2023-05-10 14:05:09 -0300

    chore: create theme configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-primary": "#D6A72F",
+        "dark-primary": "#907122",
+        "light-secondary": "#D700FF",
+        "dark-secondary": "#9807B4",
+        "gray-custom": "#717171",
+        "dark-gray-custom": "#2C2C2C",
+      },
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+        "antic-didone": ["Antic Didone", "serif"],
+      },
+      boxShadow: {
+        custom: "0 2px 15px",
+      },
+      borderRadius: {
+        custom: "4px",
+      },
+      borderWidth: {
+        custom: "1px",
+      },
+    },
   },
   plugins: [],
 };
