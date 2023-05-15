164b9b403c31d01047970c0d6fc612b4c468dee9
Felipe <pipecalgaro@gmail.com>
2023-05-15 20:26:44 -0300

    feat: implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+
 function App() {
   return (
-    <h1 className='text-purple-800'>Hello</h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      {/* <Route path='*' */}
+    </Routes>
   )
 }
 
