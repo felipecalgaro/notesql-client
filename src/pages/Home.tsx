2c64620e311b62f2699c4b7feedacf3879a15607
Felipe <pipecalgaro@gmail.com>
2023-05-20 14:01:09 -0300

    fix: update Home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
 import NotesImage from '../assets/notes.svg'
+import Button from '../components/Button'
 import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div className='bg-dark-gray-custom h-screen'>
       <Header />
-      <main className='flex justify-center items-center flex-wrap xl:gap-x-60 gap-x-20 h-4/5 mx-16'>
-        <div className='flex flex-col justify-evenly xs:h-2/3 h-1/2 w-min'>
-          <div className='w-1/2 flex items-center whitespace-nowrap'>
-            <p className='text-light-primary font-antic-didone sm:text-7xl xs:text-6xl text-5xl'>Organization &<br />Productivity</p>
+      <main className='flex justify-center items-center flex-wrap xl:gap-x-60 gap-x-20 h-4/5 xs:mx-16 mx-10'>
+        <div className='flex flex-col justify-evenly xs:h-2/3 h-1/2 xs:w-min w-auto'>
+          <div className='xs:w-1/2 w-full flex items-center xs:justify-start justify-center whitespace-nowrap'>
+            <p className='text-light-primary font-antic-didone sm:text-7xl xs:text-6xl text-4xl'>Organization &<br />Productivity</p>
           </div>
-          <div className='flex items-center justify-between sm:w-4/5 w-11/12'>
-            <p className='text-white sm:text-xl xs:text-lg text-base w-2/3'>Your new best choice to take notes the easiest way.</p>
-            <button className='border-light-primary border-custom shadow-light-primary/25 shadow-custom rounded-custom text-light-primary px-3 py-1 sm:text-xl xs:text-lg text-base hover:bg-light-primary hover:text-black transition-colors duration-300 active:bg-dark-primary active:shadow-none active:border-dark-primary active:scale-[0.98]'>Sign Up</button>
+          <div className='flex items-center xs:justify-between justify-center xs:w-11/12 w-full gap-x-2 gap-y-4 flex-wrap'>
+            <p className='text-white xs:text-start text-center sm:text-xl xs:text-lg text-base xs:w-2/3 w-5/6'>Your new best choice to take notes the easiest way.</p>
+            <Button isOutline={true} palette='primary' text='Sign Up' />
           </div>
         </div>
         <div className='basis-80'>
