af311dfe3f2c58ee63db30742ba5ce50dbf19cd3
Felipe <pipecalgaro@gmail.com>
2023-07-25 11:38:27 -0300

    feat: implement new routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import MyNotes from './pages/MyNotes'
 import WriteNote from './pages/WriteNote'
+import MyAccount from './pages/MyAccount'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/my-notes/:userId' element={<MyNotes />} />
       <Route path='/write-note/:userId' element={<WriteNote />} />
-      {/* <Route path='*' */}
+      <Route path='/my-account/:userId' element={<MyAccount />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   )
 }
