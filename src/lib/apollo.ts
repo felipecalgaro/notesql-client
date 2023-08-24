3bbab6f392f86371a156cb7b18caf301c3759b95
Felipe <pipecalgaro@gmail.com>
2023-08-24 19:29:40 -0300

    fix: update apollo client uri property

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URI ?? "http://localhost:4000/",
+  uri:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:4000/"
+      : import.meta.env.VITE_API_URI,
   cache: new InMemoryCache(),
 });
