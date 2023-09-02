29eb7bee0b7235725b4b2a2c1b7d578ce93ee491
Felipe <pipecalgaro@gmail.com>
2023-09-02 10:45:05 -0300

    fix: change component layout

--- src/components/InfoField.tsx
+++ src/components/InfoField.tsx
@@ -5,9 +5,9 @@ interface InfoFieldProps {
 
 export default function InfoField({ fieldName, fieldValue }: InfoFieldProps) {
   return (
-    <div className='border-b-custom border-gray-custom flex justify-between items-center py-4 px-6 w-2/3 text-white text-2xl font-light'>
+    <div className='border-b-custom border-gray-custom flex justify-between items-center py-4 px-6 gap-x-12 w-2/3 text-white xs:text-2xl text-lg font-light'>
       <p>{fieldName}</p>
-      <p>{fieldValue}</p>
+      <p className='overflow-x-auto scrollbar-custom'>{fieldValue}</p>
     </div>
   )
 }
